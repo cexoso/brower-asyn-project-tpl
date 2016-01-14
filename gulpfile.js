f6f2f2634279e97844689af6c377ccda15b0991c
cexoso <285516193@qq.com>
2016-01-14 22:57:10 +0800

    nd

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
-var browerAsyn=require("browser-sync");
+var browserSync=require("browser-sync");
+var gulp= require('gulp');
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        middleware: [function (req, res, next) {
+          console.log(123)
+          next();
+        }]
+    });
+});
+gulp.task("default",['browser-sync']);
