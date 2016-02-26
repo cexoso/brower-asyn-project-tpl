e6c0ca6af34b9c3559e5c5899ac51b45dbbc0f82
cexoso <285516193@qq.com>
2016-02-26 18:15:55 +0800

    nd

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,36 @@
 var browserSync=require("browser-sync");
 var gulp= require('gulp');
-gulp.task('browser-sync', function() {
+var sass=require("gulp-sass");
+var autoprefixer = require('gulp-autoprefixer');
+var chokidar = require('chokidar');
+gulp.task('browser-sync',["watch"],function() {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: ["app"]
         },
         middleware: [function (req, res, next) {
-          console.log(123)
           next();
-        }]
+        }],
+        port:80
+    });
+});
+gulp.task("watch",function(){
+  chokidar.watch('app/**/*.scss')
+    .on("change",function(path){
+      gulp.src(path, {
+          base: '.'
+      })
+      .pipe(sass()
+          .on('error', sass.logError))
+      .pipe(autoprefixer({
+          browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./'))
+      .pipe(browserSync.stream());
+    });
+  chokidar.watch('app/**/*.html')
+    .on("change",function(path){
+      browserSync.reload();
     });
 });
 gulp.task("default",['browser-sync']);
